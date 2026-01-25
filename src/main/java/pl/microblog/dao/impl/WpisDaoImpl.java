0efa6648f37da78a17c853382709950ffa56cede
TBCoW <thebluecatofwater@gmail.com>
2026-01-25 22:05:13 +0100

    Implementacja DAO i drobne poprawki
    Zaimplementowanie wszystkich wymaganych metod DAO wraz z interfejsami.
    
    mvn clean install działa poprawnie.
    
    poprawki:
    -zmiana double na LocalDateTime przy czasie
    -usunięcie zbędnego userID przy followerze (były 3 różne ID, teraz są 2)
    -zmiana sprawdzenia pliku źródłowego i przez to lekkie zmiany w package

---
+++ src/main/java/pl/microblog/dao/impl/WpisDaoImpl.java
@@ -0,0 +1,57 @@
+package main.java.pl.microblog.dao.impl;
+
+import main.java.pl.microblog.dao.WpisDao;
+import main.java.pl.microblog.model.Wpis;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SuppressWarnings("unchecked")
+@Transactional
+public class WpisDaoImpl implements WpisDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Wpis> wezTimelineUzytkownika(int userId) {
+        String queryString = "SELECT w FROM Wpis w WHERE w.userID = :userId ORDER BY w.dataDodania DESC";
+        Query query = entityManager.createQuery(queryString);
+        query.setParameter("userID", userId);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Wpis> wezFullTimelineUzytkownika(int userId) {
+        String queryString = "SELECT DISTINCT w FROM Wpis w LEFT JOIN Follower f ON f.followingID = w.userID " +
+                "AND f.followerID = :userId WHERE w.userID = :userId OR f.id IS NOT NULL ORDER BY w.dataDodania DESC";
+        Query query = entityManager.createQuery(queryString);
+        query.setParameter("userID", userId);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Wpis> wezFullPublicTimeline() {
+        String queryString = "SELECT w FROM Wpis w ORDER BY w.dataDodania DESC";
+        Query query = entityManager.createQuery(queryString);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public void dodajWpis(int userId, String tresc, LocalDateTime dataDodania, int liczbaLajkow) {
+        Wpis wpis = new Wpis();
+        wpis.setUserID(userId);
+        wpis.setTresc(tresc);
+        wpis.setDataDodania(dataDodania);
+        wpis.setLiczbaLajkow(liczbaLajkow);
+
+        entityManager.persist(wpis);
+    }
+}
