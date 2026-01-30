bfc1d6886eb2b6e4ab6b0017e5af6279a1ee4e00
TBCoW <81047842+TBCoW@users.noreply.github.com>
2026-01-30 18:43:57 +0100

    Revert "Pierwszy test"

--- src/main/java/pl/microblog/dao/impl/WpisDaoImpl.java
+++ src/main/java/pl/microblog/dao/impl/WpisDaoImpl.java
@@ -1,7 +1,7 @@
-package pl.microblog.dao.impl;
+package main.java.pl.microblog.dao.impl;
 
-import pl.microblog.dao.WpisDao;
-import pl.microblog.model.Wpis;
+import main.java.pl.microblog.dao.WpisDao;
+import main.java.pl.microblog.model.Wpis;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
