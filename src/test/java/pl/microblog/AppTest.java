36033758a2293fffca2e530cfc8f33bc86c423b5
MateuszKuc-ai <retro.mtq@gmail.com>
2026-01-31 17:37:39 +0100

    Stworzenie klas testowych

---
+++ src/test/java/pl/microblog/AppTest.java
@@ -0,0 +1,17 @@
+package pl.microblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext-test.xml"})
+public class AppTest {
+
+    @Test
+    public void testContext() {
+
+        System.out.println("spring test");
+    }
+}
